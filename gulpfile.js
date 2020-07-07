b7e453ad28a825d6422089c3d34f1eed3f0b7f23
leticiaoli <lesilvaoliveira03@gmail.com>
2020-07-07 19:10:58 -0300

    start projet

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+const gulp = require('gulp');
+const uglify = require('gulp-uglify');
+const minifycss = require('gulp-clean-css');
+const imagemin = require('gulp-imagemin');
+const browserSync = require('browser-sync').create();
+const concat = require('gulp-concat');
+
+
+gulp.task('minify-js', function () {
+  return gulp.src('./src/js/**/*.js')
+  .pipe(uglify())
+  .pipe(concat('script.min.js'))
+  .pipe(gulp.dest('./build/js'));
+});
+
+
+gulp.task('minify-css', function () {
+  return gulp.src('./src/css/**/*.css')
+  .pipe(minifycss({
+    compatibility: 'ie8'
+  }))
+  .pipe(gulp.dest('./build/css'));
+});
+
+
+gulp.task('image-min', function () {
+  return gulp.src('./src/img/*')
+  .pipe(imagemin({ progressive: true, optimizationLevel: 7 }))
+  .pipe(gulp.dest('./build/img'));
+});
+
+
+gulp.task('browser-sync', function() {
+  return browserSync.init({
+    watch: true,
+    server: {
+      baseDir: "./"
+    }
+  });
+
+});
+
+const watch = function () {
+  gulp.watch('./index.html').on('change', browserSync.reload);
+  gulp.watch('./src/js/**/*.js', ['minify-js']).on('change', browserSync.reload);
+  gulp.watch('./src/css/**/*.css', ['minify-css']).on('change', browserSync.reload);
+  gulp.watch('./src/img/**/*', ['image-min']).on('change', browserSync.reload);
+}
+
+
+gulp.task('default', gulp.series('image-min', 'minify-js', 'minify-css', 'browser-sync', watch));
